d14cb33e3d1ca65f44ed5bfd35a8fdf8f0a87349
Steffen Andre Longva <steffen.andrelongva@tietoevry.com>
2023-08-31 15:50:45 +0200

    updated readmes

--- oc-run/README.md
+++ oc-run/README.md
@@ -1,8 +1,10 @@
 # zsh-stuff
-Various zsh stuff
+Allows running oc command across different clusters \
+oc<clustername> get resource \
 
-Install\
-Clone the repository. git clone https://github.com/steffenlongva/zsh-stuff.git ~/.oh-my-zsh/custom/plugins \
+Install \
+Clone the repository. \
+git clone https://github.com/steffenlongva/zsh-stuff.git ~/.oh-my-zsh/custom/plugins \
 
 \
 Add oc-run to plugin list
--- ocp-limits.plugin.zsh
+++
@@ -1,8 +0,0 @@
-function ocpl() {
-  local project_name=$1
-  oc get pods -n $project_name --no-headers=true -o custom-columns="STATE:.status.phase,NAME:.metadata.name,CPU_REQ:.spec.containers[*].resources.requests.cpu,MEM_REQ:.spec.containers[*].resources.requests.memory,CPU_LIMIT:.spec.containers[*].resources.limits.cpu,MEM_LIMIT:.spec.containers[*].resources.limits.memory" \
-  | awk 'BEGIN { printf "%-9s %-50s %-9s %-9s %-11s %-11s\n", "STATE", "NAME", "CPU_REQ", "MEM_REQ", "CPU_LIMIT", "MEM_LIMIT" }
-         { printf "%-9s %-50.50s %-9s %-9s %-11s %-11s\n", $1, $2, $3, $4, $5, $6 }'
-}
-
-# Usage: ocpl <project_name>
